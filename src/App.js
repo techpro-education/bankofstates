2b9c477e9cf5bc7675efc8322b5b0591fdfd43c7
Mohammed Riyaz <jmriyaz84@gmail.com>
2020-12-20 18:35:20 +0400

    Adding State Provider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./header/Header";
+import Home from "./home/Home";
 function App() {
   return (
     <Router>
@@ -9,7 +10,7 @@ function App() {
         <Switch>
           <Route path="/login">Login</Route>
           <Route path="/register">Register</Route>
-          <Route path="/"></Route>
+          <Route path="/" component={Home} />
         </Switch>
       </div>
     </Router>
